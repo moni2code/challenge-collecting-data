c4d5eb9c027cfb00acdfdc4890b86b97598b4506
Sam Veldeman <sam.veldeman@gmail.com>
2023-06-29 00:33:08 +0200

    Creation of dictionary for 1 house complete Creation of a key_value list (excercise assignment)

--- Test.py
+++ Test.py
@@ -1,12 +1,21 @@
-import requests, re, json
 from bs4 import BeautifulSoup
-from pathlib import Path
-root_url = "https://www.immoweb.be"
-web_url = "https://www.immoweb.be/en/classified/mixed-use-building/for-sale/gent/9000/10655537"
-cookie = requests.get(root_url).cookies
-resp = BeautifulSoup(requests.get(root_url, cookies=cookie).text)
-print(resp.prettify())
-if resp == "<Response [200]>":
-    print ('OK!')
-else:
-    print ('Boo!')
+import re
+import json
+
+# Assume 'html' contains the HTML content of the webpage
+soup = BeautifulSoup(html, 'html.parser')
+
+# Find the <script> tag containing the JavaScript object
+script_tag = soup.find('script', text=re.compile(r'windows\.classified'))
+
+# Extract the JavaScript object text
+script_text = script_tag.string
+
+# Parse the JavaScript object into a Python dictionary
+data = re.search(r'(?<=windows\.classified\s=\s).*?(?=;)', script_text).group()
+data_dict = json.loads(data)
+
+# Access and process the data as needed
+# For example, printing the property features and values
+for key, value in data_dict.items():
+    print(f"Feature: {key}, Value: {value}")
--- immoweb.py
+++ immoweb.py
@@ -1,36 +1,48 @@
-import requests, re, json
+import requests
+import re
 from bs4 import BeautifulSoup
-from pathlib import Path
-session = requests.Session()
-web_url = "https://www.immoweb.be/en/classified/mixed-use-building/for-sale/gent/9000/10655537"
-#cookie = session.get(web_url).cookies
-soup = BeautifulSoup(session.get(web_url,cookies=cookie).text, "html.parser")
-for values in 
-values_interior = session.get
-def get_house_data():
-    #session = requests.Session()
-    cookie = session.get(web_url).cookies              # makes a cookies request using the session method from requests. 
-    house = session.get(web_url, cookies=cookie)  # makes house request using the session method from requests.  
-    house_data = {house: requests.get(web_url, params={"country": country}, cookies=cookie).json() for country in requests.get(urls['countries_url'], cookies=cookie).json()}
-    for country in countries.json():
-        while True:
-            try:
-                leaders = requests.get(urls['leaders_url'], params= {"country": country}, cookies=cookie)
-                if leaders.status_code == 403:  # Check if cookies have expired
-                    cookie = session.get(urls['cookie_url']).cookies  # Refresh the cookies
-                    continue
-                else:
-                    leaders = leaders.json()
-                    leaders_per_country[country] = []
-                    for leader in leaders:
-                        leader["first_paragraph"] = get_first_paragraph(leader["wikipedia_url"], session)
-                        leaders_per_country[country].append(leader)
-                        print(f"Name: {leader['first_name']} {leader['last_name']}")
-                        print(f"First Paragraph: {leader['first_paragraph']}")
-                        print()
-                break
-            except Exception as e:
-                print(f"An error occurred for country '{country}': {str(e)}")
-                break
-    return leaders_per_country
-print(soup)
+import json
+
+def get_property():
+    # Make a GET request to fetch the HTML content of the page
+    url = "https://www.immoweb.be/en/classified/mixed-use-building/for-sale/gent/9000/10655537"  # Replace with the URL of the page containing the data
+    response = requests.get(url)
+    html_content = response.text
+    # Parse the HTML content using BeautifulSoup
+    soup = BeautifulSoup(html_content, "lxml")
+    # Find the script tag containing the desired data
+    script_tag = soup.find("script", string=lambda text: text and "window.classified" in text)
+    if script_tag:
+        # Extract the data within the script tag
+        script_content = script_tag.string.strip()
+        # Remove the "window.classified =" prefix and removing all the whitespace and clutter in the suffix to get only the dictionary to parse into the JSON_data.
+        pattern = r"window\.classified = ({.*?});"
+        match = re.search(pattern, script_content)
+    if match:
+        json_data = match.group(1)
+        # Load the JSON data into a dictionary
+        house_dict = json.loads(json_data)
+        # Clean the data in the dictionary by key values
+        keys_to_delete = ["cluster", "customers", "features", "premiumProjectPage", "media", "alternativeDescriptions", "hasSectionsArray", "displayFlags"]
+        house_dict = {key: value for key, value in house_dict.items() if key not in keys_to_delete}
+    return house_dict
+# Function to itterate over all nested dictionaries in house_dict
+def extract_nested_values(data, keys_to_extract, prefix=""):
+    results = []
+    if isinstance(data, dict):
+        for key, value in data.items():
+            new_prefix = f"{prefix}.{key}" if prefix else key
+            if key in keys_to_extract:
+                results.append((new_prefix, value))
+            results.extend(extract_nested_values(value, keys_to_extract, prefix=new_prefix))
+    return results
+house_dict = get_property()
+# Multiple "type" keys in the dictionary
+kitchen_type = house_dict['property']['kitchen']['type']
+#list of key values for the excercise asignment
+list_key_values =[
+    "locality", "type", "subtype", "mainValue", "type_of_sale", "bedroomCount",
+    "netHabitableSurface", kitchen_type, "isFurnished","fireplaceExists", "hasTerrace",
+    "hasgarden", "surface", "facadeCount", "hasSwimmingPool","condition"]
+key_values = extract_nested_values(house_dict, list_key_values)
+print(key_values)
